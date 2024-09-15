b9ef4c387226aeff27effcca8da0980a5e564759
unknown <monnkkey2004@gmail.com>
2024-09-15 17:42:32 +0700

    fisrt

---
+++ src/main/java/murach/email/TestServlet.java
@@ -0,0 +1,30 @@
+package murach.email;
+
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
+
+@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>TestServlet Get</h1>");
+        out.println("</body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>TestServlet Post</h1>");
+        out.println("</body></html>");
+    }
+}
